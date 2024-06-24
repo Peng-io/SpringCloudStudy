d3c34fcb8198886a2cd407ea8c314b23a9eff920
19442 <1944282957@qq.com>
2024-06-24 16:13:55 +0800

    study

---
+++ borrow-service/src/main/java/com/test/controller/BorrowController.java
@@ -0,0 +1,30 @@
+package com.test.controller;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.test.entity.UserBorrowDetail;
+import com.test.service.BorrowService;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.Collections;
+
+@RestController
+public class BorrowController {
+
+    @Resource
+    BorrowService service;
+
+    //@HystrixCommand(fallbackMethod = "onError")
+    @RequestMapping("/borrow/{uid}")
+    UserBorrowDetail findUserBorrows(@PathVariable("uid") int uid) {
+        return service.getUserBorrowDetailByUid(uid);
+    }
+
+    //备选方案，这里直接返回空列表了
+    //注意参数和返回值要和上面的一致
+    UserBorrowDetail onError(int uid) {
+        return new UserBorrowDetail(null, Collections.emptyList());
+    }
+}
---
+++ borrow-service/src/main/java/com/test/service/impl/BorrowServiceImpl.java
@@ -0,0 +1,45 @@
+package com.test.service.impl;
+
+import com.test.client.BookClient;
+import com.test.client.UserClient;
+import com.test.entity.Book;
+import com.test.entity.Borrow;
+import com.test.entity.User;
+import com.test.entity.UserBorrowDetail;
+import com.test.mapper.BorrowMapper;
+import com.test.service.BorrowService;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class BorrowServiceImpl implements BorrowService {
+
+    @Resource
+    BorrowMapper mapper;
+
+    @Resource
+    RestTemplate template;
+
+    @Resource
+    UserClient userClient;
+    @Resource
+    BookClient bookClient;
+
+    @Override
+    public UserBorrowDetail getUserBorrowDetailByUid(int uid) {
+        List<Borrow> borrow = mapper.getBorrowsByUid(uid);
+        //这里通过调用getForObject来请求其他服务，并将结果自动进行封装
+        //获取User信息
+        User user = userClient.getUserById(uid);
+        //获取每一本书的详细信息
+        List<Book> bookList = borrow
+                .stream()
+                .map(b -> bookClient.findBookById(b.getBid()))
+                .collect(Collectors.toList());
+        return new UserBorrowDetail(user, bookList);
+    }
+}
